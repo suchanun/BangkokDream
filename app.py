b28e1c45521233c95c1385a0bcd62d07722fa1b7
Suchanun Piriyasatit <s.piriyasatit@gmail.com>
2020-09-12 21:58:04 +0700

    sub budget

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ import pandas as pd
 import json
 import plotly.graph_objects as go
 import config
+import numpy as np
 
 def main():
-    st.sidebar.selectbox('mode',['hi','hello'])
+
+
     data = load_data()
     config.bkk_coordinates = data['bkk_coordinates']  # boundaries of bangkok
     config.df_district_loc = data['df_district_loc']  # one point location of each district # modified
@@ -14,9 +16,59 @@ def main():
     config.df_subbudget = data['df_subbudget']  # mine
     config.df_budget_district = data['df_budget_district']#config.df_budget[pd.notnull(config.df_budget.lat)]
 
+    districts = sorted([district for district in config.df_budget.budget.unique() if district.startswith('สำนักงานเขต')])
+    district = st.selectbox('สำนักงานเขต', districts, format_func=lambda dep: dep[11:])
+
+    # plot main map
     bangkok_fig = get_fig_bangkok()
     st.plotly_chart(bangkok_fig)
 
+    # choose subbudget of district
+
+    # plot pie district
+
+    df_subbudget = config.df_subbudget  # [config.df_subbudget[]]
+    print('district == ')
+    print(district)
+    df_subbudget = df_subbudget[df_subbudget.budget == district]
+    print(df_subbudget.shape)
+
+
+    # st.selectbox('แผนงาน', )
+
+    subbudgets = df_subbudget.sub_budget
+    subbudget = st.selectbox('แผนงาน', list(subbudgets))
+    district_pie = get_pie_district(df_subbudget, subbudget)
+    st.plotly_chart(district_pie)
+
+
+
+def get_pie_district(df_subbudget_of_district,subbudget=None):
+   # df_subbudget = config.df_subbudget #[config.df_subbudget[]]
+    df_subbudget = df_subbudget_of_district#df_subbudget[df_subbudget.budget==district]
+
+    subbudgets = list(df_subbudget.sub_budget)
+    labels = subbudgets
+    values = df_subbudget.amount#[4500, 2500, 1053, 500]
+    n_subbudget = len(values)
+    pull=None
+
+    if subbudget:
+        target_idx = [i for i in range(n_subbudget) if subbudgets[i] == subbudget][0]
+        pull = np.zeros(n_subbudget)
+        pull[target_idx] = 0.2
+
+
+    # pull is given as a fraction of the pie radius
+    fig = go.Figure(data=[go.Pie(
+                labels=labels, values=values,textinfo='label+percent',
+                pull=pull,
+                #insidetextorientation='radial'?????
+    )])#, pull=[0, 0, 0.2, 0]
+
+
+    fig.update_layout(showlegend=False)
+    return fig
 
 def get_fig_bangkok():
     data = list()
@@ -71,6 +123,7 @@ def load_data():
     data['df_budget'] = pd.read_pickle('./data-nice/df_budget.pkl')  # mine
     data['df_subbudget'] = pd.read_pickle('./data-nice/sub_budget.pkl')  # mine
     data['df_budget_district'] = data['df_budget'][pd.notnull(data['df_budget'].lat)]
+
     return data
 
 if __name__ == "__main__":
